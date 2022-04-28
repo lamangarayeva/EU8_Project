04ff3a54a53ce2355bc88d19b1ee48875bb42c4b
Yalcin YILDIZ <64442783+yyildizz@users.noreply.github.com>
2022-04-28 23:18:14 +0200

    completed us102

--- src/test/java/us100/TestCase_001.java
+++ src/test/java/us100/TestCase_001.java
@@ -2,6 +2,9 @@ package us100;
 
 public class TestCase_001 {
     public static void main(String[] args) {
-        System.out.println("Test Case 001 started");
+        System.out.println(":added some more codes bla bla bla");
+        System.out.println("Test Case 001 completed");
+
+
     }
 }
